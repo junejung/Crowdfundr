cb9b62b2076b590be93671d1215f4ec9106fdebe
June Jung <github.com@jueunjung.com>
2021-11-11 20:28:34 -0500

    cleaning up tests

--- test/project.test.js
+++ test/project.test.js
@@ -5,12 +5,15 @@ const { ethers } = require("hardhat");
 
 
 describe("Crowdfundr Contract", function () {
+
   beforeEach( async function() {
     Crowdfundr = await ethers.getContractFactory("Crowdfundr");
     [alex, diana, gab] = await ethers.getSigners();
     crowdfundr = await Crowdfundr.deploy();
+
     await crowdfundr.deployed();
   });
+
   it("Should be able to register multiple projects", async function () {
     await crowdfundr.connect(alex).createProject(parseEther('20'));
     await crowdfundr.connect(diana).createProject(parseEther('20'));
@@ -27,13 +30,17 @@ describe("Crowdfundr Contract", function () {
     let projects = await crowdfundr.getAllProjects();
     expect(projects.length).to.deep.equal(2);
   });
-})
+});
 
 describe("Project Contract", function () {
+  let aMinContribution = parseEther("0.01");
+  let aGoalAmount = parseEther("20");
+  let someAmount = parseEther("15");
+
   beforeEach( async function() {
     Project = await ethers.getContractFactory("Project");
-    [owner, addr2, addr3, ...addrs] = await ethers.getSigners();
-    project = await Project.deploy(owner.address, parseEther("20"));
+    [owner, gilbert, prolok] = await ethers.getSigners();
+    project = await Project.deploy(owner.address, aGoalAmount);
 
     await project.deployed();
   });
@@ -44,69 +51,58 @@ describe("Project Contract", function () {
   });
 
   describe("invest", function() {
-    it("Should increased balance after it went through successfully", async function () {
-      const aContribution = parseEther("0.01");
 
-      await project.invest({value : aContribution }); 
-      expect(await project.balance()).to.deep.equal(aContribution);
+    it("Should increased balance after it went through", async function () {
+      await project.invest({value : aMinContribution });
 
+      expect(await project.balance()).to.deep.equal(aMinContribution);
     });
 
     it("Should throw an error when its amount lower than the minimum requirement", async function () {
-      const smallContribution = parseEther("0.001");
+      let aTooSmallContribution = parseEther("0.001");
 
-      await expect(project.invest({ value : smallContribution })).to.be.revertedWith('LOWER_THAN_REQUIRE_MIN');
+      await expect(project.invest({ value : aTooSmallContribution })).to.be.revertedWith('LOWER_THAN_REQUIRE_MIN');
     })
 
     it("Should fail when the goal already met", async function () {
-      const aLargeContribution = parseEther("20");
-      const aContribution = parseEther("0.01");
+      await project.invest({value : aGoalAmount });
 
-      await project.invest({value : aLargeContribution }); 
-      await expect(project.invest({ value : aContribution })).to.be.reverted;
-
+      await expect(project.invest({ value : aMinContribution })).to.be.reverted;
     });
 
     it("Should fail if the project ended", async function () {
-      const aContribution = parseEther("0.01");
+      await project.invest({value : aMinContribution });
 
-      //invest before project ends
-      await project.invest({value : aContribution });
-      //passing 30days 
       await network.provider.send("evm_increaseTime", [86400000 * 30]); //30 days in millisecond
       await network.provider.send("evm_mine");
-      //expect failing the invest attempt after end date passes
-      await expect(project.invest({ value : aContribution })).to.be.reverted;
-      //expect only the first investment went through
-      expect(await project.balance()).to.deep.equal(aContribution);
 
+      await expect(project.invest({ value : aMinContribution })).to.be.reverted;
+      expect(await project.balance()).to.deep.equal(aMinContribution);
     });
 
     it("Should fail if the project cancelled", async function () {
-      const aContribution = parseEther("0.01");
-
       await project.connect(owner).cancel();
-      await expect(project.connect(addr2).invest({ value : aContribution })).to.be.reverted;
+
+      await expect(project.connect(gilbert).invest({ value : aMinContribution })).to.be.reverted;
     });
   });
 
   describe("withdraw", function() {
 
     it("Should fail when withdraw isn't the owner of the project", async function () {
-      const goalAmountContribution = parseEther("20");
+      await project.invest({value : aGoalAmount });
+      await expect(project.connect(gilbert).withdraw(aGoalAmount)).to.be.revertedWith('NOT_OWNER');
 
-      await project.invest({value : goalAmountContribution });
-      await expect(project.connect(addr2).withdraw(goalAmountContribution)).to.be.revertedWith('NOT_OWNER');
-      expect(await project.balance()).to.deep.equal(goalAmountContribution);
+      expect(await project.balance()).to.deep.equal(aGoalAmount);
     });
 
     it("Should be successful when the owner of project request it after project is successful", async function () {
-      const goalAmountContribution = parseEther("20");
-      await project.invest({value : goalAmountContribution });
+      await project.invest({value : aGoalAmount });
 
       let balanceBefore = await hre.ethers.provider.getBalance(owner.address);
-      await project.connect(owner).withdraw(goalAmountContribution);
+      await project.connect(owner).withdraw(aGoalAmount);
       let balanceAfter = await hre.ethers.provider.getBalance(owner.address);
+
       expect(Number(balanceBefore) < Number(balanceAfter)).to.deep.equal(true);
     });
   });
@@ -114,48 +110,42 @@ describe("Project Contract", function () {
   describe("refund", function() {
 
     it("Should be sccessful when project is failed", async function() {
-      const aContribution = parseEther("15");
-      await project.connect(addr2).invest({value : aContribution });
+      await project.connect(prolok).invest({value : someAmount });
 
-      let balanceBefore = await hre.ethers.provider.getBalance(addr2.address);
+      let balanceBefore = await hre.ethers.provider.getBalance(prolok.address);
 
       await network.provider.send("evm_increaseTime", [86400000 * 31]); //31 days in millisecond
       await network.provider.send("evm_mine");
 
-      await project.connect(addr2).refund();
-      let balanceAfter = await hre.ethers.provider.getBalance(addr2.address);
-      expect(Number(balanceBefore) < Number(balanceAfter)).to.deep.equal(true);
+      await project.connect(prolok).refund();
 
+      let balanceAfter = await hre.ethers.provider.getBalance(prolok.address);
+      expect(Number(balanceBefore) < Number(balanceAfter)).to.deep.equal(true);
     });
 
     it("Should be sccessful when project is cancelled", async function() {
-      const aContribution = parseEther("15");
-      await project.connect(addr2).invest({value : aContribution });
+      await project.connect(prolok).invest({value : someAmount });
 
-      let balanceBefore = await hre.ethers.provider.getBalance(addr2.address);
-
+      let balanceBefore = await hre.ethers.provider.getBalance(prolok.address);
       await project.connect(owner).cancel();
+      await project.connect(prolok).refund();
+      let balanceAfter = await hre.ethers.provider.getBalance(prolok.address);
 
-      await project.connect(addr2).refund();
-      let balanceAfter = await hre.ethers.provider.getBalance(addr2.address);
       expect(Number(balanceBefore) < Number(balanceAfter)).to.deep.equal(true);
-
     });
   });
 
   describe("cancel", function() {
 
     it("Should be sccessful when the owner of project request before end of the project deadline", async function() {
-      const aContribution = parseEther("15");
-      await project.connect(addr2).invest({value : aContribution });
+      await project.connect(gilbert).invest({value : someAmount });
 
       let cancelation = await project.connect(owner).cancel();
       expect(cancelation).to.emit(project, 'projectCancelled').withArgs(owner.address);
     });
 
     it("Should fail when owner request it after deadline passed", async function() {
-      const aContribution = parseEther("15");
-      await project.connect(addr2).invest({value : aContribution });
+      await project.connect(gilbert).invest({value : someAmount });
 
       await network.provider.send("evm_increaseTime", [86400000 * 31]); //31days in millisecond
       await network.provider.send("evm_mine");
@@ -164,29 +154,30 @@ describe("Project Contract", function () {
     });
 
     it("Should fail when other than the owner request it", async function() {
-      const aContribution = parseEther("15");
-      await project.connect(addr2).invest({value : aContribution });
+      await project.connect(gilbert).invest({value : someAmount });
 
-      await expect(project.connect(addr2).cancel()).to.be.revertedWith('NOT_OWNER');
+      await expect(project.connect(gilbert).cancel()).to.be.revertedWith('NOT_OWNER');
     });
   });
 
   describe("reward", function() {
-    it("Should be given when contributor made 1ETH countribution", async function () {
-      const aNFTContributionAmount = parseEther("1");
+    let aNFTContributionAmount = parseEther("1");
 
-      let awardGiven = await project.connect(addr2).invest({ value : aNFTContributionAmount });
-      expect(awardGiven).to.emit(project, 'RewardGiven').withArgs(addr2.address, 1);
+    it("Should be given when contributor made 1ETH countribution", async function () {
+      let awardGiven = await project.connect(prolok).invest({ value : aNFTContributionAmount });
+
+      expect(awardGiven).to.emit(project, 'RewardGiven').withArgs(prolok.address, 1);
     });
 
     it("Should be given more than 1 per 1 ETH when contributor made contribution", async function () {
-      const firstContribution = parseEther("0.9");
-      const secondContribution = parseEther("2.6");
+      let firstContribution = parseEther("0.9");
+      let secondContribution = parseEther("2.6");
 
-      let firstInvestment = await project.connect(addr2).invest({ value : firstContribution });
-      expect(firstInvestment).to.emit(project, 'RewardGiven').withArgs(addr2.address, 0);
-      let secondInvestment = await project.connect(addr2).invest({ value : secondContribution });
-      expect(secondInvestment).to.emit(project, 'RewardGiven').withArgs(addr2.address, 3);
+      let firstInvestment = await project.connect(prolok).invest({ value : firstContribution });
+      expect(firstInvestment).to.emit(project, 'RewardGiven').withArgs(prolok.address, 0);
+
+      let secondInvestment = await project.connect(prolok).invest({ value : secondContribution });
+      expect(secondInvestment).to.emit(project, 'RewardGiven').withArgs(prolok.address, 3);
     });
   });
 });

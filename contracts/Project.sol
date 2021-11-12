171e4501433bc43042fb15e27998409af79710aa
June Jung <github.com@jueunjung.com>
2021-11-11 20:50:09 -0500

    adding _send to use call instead of transfer

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -42,7 +42,6 @@ contract Project is ERC721 {
         owner = _owner;
         goalAmount = _goal;
         minAmount = 0.01 ether;
-        balance = 0.00 ether;
         endDate = block.timestamp + 30 days;
         state = State.ONGOING;
     }
@@ -67,13 +66,12 @@ contract Project is ERC721 {
     }
 
     function _mint(address _recipient) internal returns (uint256) {
-        _tokenIdCounter++;
-        _safeMint(_recipient, _tokenIdCounter);
+        _safeMint(_recipient, _tokenIdCounter++);
 
         return _tokenIdCounter;
     }
 
-    function reward(address _recipient) internal inState(State.ONGOING) {
+    function _reward(address _recipient) internal inState(State.ONGOING) {
         uint nftsToGive = (contributions[_recipient] - givenNFTs[_recipient].length) / (1 ether);
         for (uint i; i < nftsToGive; i++) {
             givenNFTs[_recipient].push(_mint(_recipient));
@@ -82,13 +80,18 @@ contract Project is ERC721 {
         emit RewardGiven(_recipient, givenNFTs[_recipient].length);
     }
 
+    function _send(address _to, uint amount) public payable {
+        (bool result, ) = _to.call{value: amount}("");
+        require(result, "TRANSACTION_FAILED");
+    }
+
     function invest() external expired(false) inState(State.ONGOING) payable {
         require(msg.value >= minAmount, 'LOWER_THAN_REQUIRE_MIN');
 
         balance += msg.value;
         contributions[msg.sender] += msg.value;
 
-        reward(msg.sender);
+        _reward(msg.sender);
 
         if (balance >= goalAmount) {
             state = State.SUCCESS;
@@ -96,21 +99,24 @@ contract Project is ERC721 {
     }
 
     function withdraw(uint _requestAmount) external inState(State.SUCCESS) restricted payable {
-        payable(msg.sender).transfer(_requestAmount);
         balance = balance - _requestAmount;
+
+        _send(payable(msg.sender), _requestAmount);
     }
 
     function refund() external expired(true) inState(State.FAIL) payable {
         require(contributions[msg.sender] > 0, 'NON_SUFFICIENT_FUNDS');
 
         uint amountToRefund = contributions[msg.sender];
-        payable(msg.sender).transfer(amountToRefund);
         balance -= amountToRefund;
         contributions[msg.sender] = 0;
+
+         _send(payable(msg.sender), amountToRefund);
     }
 
     function cancel() external expired(false) restricted {
         state = State.FAIL;
+
         emit projectCancelled(msg.sender);
     }
 
